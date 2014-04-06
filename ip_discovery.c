a1265409ebf9d3f61e4650d66532edcce8ea356c
Markus Teich <markus.teich@stusta.mhn.de>
2014-04-06 17:12:05 +0200

    Step 3 done. It werks and is epic.

--- ip_discovery.c
+++ ip_discovery.c
@@ -1,5 +1,6 @@
 #define __USE_BSD
 
+#include <time.h>
 #include <arpa/inet.h>
 #include <errno.h>
 #include <linux/if.h>
@@ -7,6 +8,7 @@
 #include <net/ethernet.h>
 #include <net/if_arp.h>
 #include <netinet/if_ether.h>
+#include <netinet/ip_icmp.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -17,32 +19,50 @@
 
 typedef struct ether_header ether_header;
 typedef struct ether_arp ether_arp;
-typedef struct iphdr iphdr;
+typedef struct ip ip;
 typedef struct icmphdr icmphdr;
 
-/* BEWARE: The interface has to be up for this to work */
+/* BEWARE: The eth0 interface has to be up for this to work */
+
+/* Checksum function */
+uint16_t checksum(uint16_t *addr, int len) {
+	int nleft = len;
+	int sum = 0;
+	uint16_t *w = addr;
+	uint16_t answer = 0;
+	while(nleft > 1) {
+		sum += *w++;
+		nleft -= sizeof(uint16_t);
+	}
+	if(nleft == 1) {
+		*(uint8_t*)(&answer) = *(uint8_t*)w;
+		sum += answer;
+	}
+	sum = (sum >> 16) + (sum & 0xFFFF);
+	sum += (sum >> 16);
+	return ~sum;
+}
 
 int main(int argc, char* argv[]) {
 	int sock;
+	int i;
 	uint8_t subnet_id = 0;
-	ether_header* eth_header;
-	ether_arp* arp_header;
-	iphdr* ip_header;
-	icmphdr* icmp_header;
-	int ifindex = 0;
 	uint8_t ether_frame[ETH_FRAME_LEN];
+	ether_header* eth_header = (ether_header*)ether_frame;
+	ether_arp* arp_header    = (ether_arp*)(ether_frame + sizeof(ether_header));
+	ip* ip_header            = (ip*)(ether_frame + sizeof(ether_header));
+	icmphdr* icmp_header     = (icmphdr*)(ether_frame + sizeof(ether_header) + sizeof(ip));
+	uint8_t* icmp_payload    = (uint8_t*)(ether_frame + sizeof(ether_header) + sizeof(ip) + sizeof(icmphdr));
+	char* payload_data = "Get on your knees to honor the StuStaNet analdildos entrance!!!!";
+	int payload_len = strlen(payload_data);
+	int ifindex = 0;
 	struct ifreq ifr;
 	struct sockaddr_ll socket_address;
 	uint8_t src_mac[ETH_ALEN];
 	uint8_t dst_mac[ETH_ALEN];
 	uint8_t sw_mac[ETH_ALEN];
 	uint8_t src_ip[4] = {10, 150, 0, 240};
-	uint8_t dst_ip[4] = {10, 150, 0, 254};
-
-	eth_header  = (ether_header*)ether_frame;
-	arp_header  = (ether_arp*)(ether_frame + sizeof(ether_header));
-	ip_header   = (iphdr*)(ether_frame + sizeof(ether_header));
-	icmp_header = (icmphdr*)(ether_frame + sizeof(ether_header) + sizeof(ip_header));
+	uint8_t sw_ip[4] = {10, 150, 0, 254};
 
 	/* Open raw socket (needs root) to listen for arp */
 	if((sock = socket(AF_PACKET, SOCK_RAW, htons(ETH_P_ALL))) == -1) {
@@ -75,7 +95,7 @@ int main(int argc, char* argv[]) {
 	 * entry for 10.150.x.240, but this address is not used and Wolfi approved
 	 * using it. */
 	memset(ether_frame, 0, ETH_FRAME_LEN);
-	src_ip[2] = dst_ip[2] = subnet_id;
+	src_ip[2] = sw_ip[2] = subnet_id;
 
 	/* retrieve ethernet interface index */
 	strncpy(ifr.ifr_name, "eth0", IFNAMSIZ);
@@ -119,7 +139,7 @@ int main(int argc, char* argv[]) {
 	arp_header->ea_hdr.ar_pln = 4;
 	arp_header->ea_hdr.ar_op = htons(ARPOP_REQUEST);
 	memcpy(arp_header->arp_tha, dst_mac, ETH_ALEN);
-	memcpy(arp_header->arp_tpa, dst_ip, 4);
+	memcpy(arp_header->arp_tpa, sw_ip, 4);
 	memcpy(arp_header->arp_sha, src_mac, ETH_ALEN);
 	memcpy(arp_header->arp_spa, src_ip, 4);
 
@@ -129,6 +149,79 @@ int main(int argc, char* argv[]) {
 		goto fail;
 	}
 	fprintf(stderr, "Sent ARP request to gateway, waiting for reply...\n");
+
+	/* Get the gateways MAC address from its ARP reply*/
+	do {
+		memset(ether_frame, 0, ETH_FRAME_LEN);
+		if(recv(sock, ether_frame, ETH_FRAME_LEN, 0) == -1) {
+			if(errno == EINTR) {
+				continue;
+			} else {
+				perror("recv() failed");
+				goto fail;
+			}
+		}
+	} while(ntohs(eth_header->ether_type) != ETHERTYPE_ARP
+	     || ntohs(arp_header->ea_hdr.ar_op) != ARPOP_REPLY
+	     || memcmp(arp_header->arp_spa, sw_ip, 4) != 0);
+	memcpy(sw_mac, arp_header->arp_sha, ETH_ALEN);
+	fprintf(stderr, "Got ARP reply from gateway at %02x:%02x:%02x:%02x:%02x:%02x\n",
+	        sw_mac[0], sw_mac[1], sw_mac[2], sw_mac[3], sw_mac[4], sw_mac[5]);
+
+	/* Step 3: send a ping from each valid /29 to determine the right one.
+	 * The switch will only pass IP packets with the correct source IP through
+	 * to the gateway. Therefore we can identify the correct /29 subnet with the
+	 * one reply, we should get. */
+	memset(ether_frame, 0, ETH_FRAME_LEN);
+
+	/* fill icmp payload */
+	memcpy(icmp_payload, payload_data, payload_len);
+
+	/* fill icmp header */
+	icmp_header->type = ICMP_ECHO;
+	icmp_header->code = 0;
+
+	/* fill ip header */
+	ip_header->ip_hl = sizeof(ip) / sizeof(uint32_t);
+	ip_header->ip_v = 4;
+	ip_header->ip_tos = 0;
+	ip_header->ip_len = htons(sizeof(ip) + sizeof(icmphdr) + payload_len);
+	ip_header->ip_off = 0;
+	ip_header->ip_ttl = 255;
+	ip_header->ip_p = IPPROTO_ICMP;
+	memcpy(&(ip_header->ip_dst), sw_ip, 4);
+
+	/* fill ethernet header */
+	memcpy(eth_header->ether_shost, src_mac, ETH_ALEN);
+	memcpy(eth_header->ether_dhost, sw_mac, ETH_ALEN);
+	eth_header->ether_type = htons(ETH_P_IP);
+
+	/* fix socket_address content */
+	memcpy(socket_address.sll_addr, sw_mac, ETH_ALEN);
+
+	/* generate 29 icmp echo requests */
+	srand(time(NULL));
+	for(i=0; i<29; i++) {
+		/* fill icmp header */
+		icmp_header->un.echo.id = rand() & 0xFFFF;
+		icmp_header->un.echo.sequence = rand() & 0xFFFF;
+		memset(&(icmp_header->checksum), 0, 2);
+		icmp_header->checksum = checksum((uint16_t*)icmp_header, payload_len + sizeof(icmphdr));
+
+		/* fill ip header */
+		ip_header->ip_id = rand() & 0xFFFF;
+		src_ip[3] = 8 + 8 * i;
+		memcpy(&(ip_header->ip_src), src_ip, 4);
+		memset(&(ip_header->ip_sum), 0, 2);
+		ip_header->ip_sum = checksum((uint16_t*)ip_header, sizeof(ip));
+
+		/* send our carefully crafted ping packet */
+		if(sendto(sock, ether_frame, sizeof(ether_header) + sizeof(ip) + sizeof(icmphdr) + payload_len, 0, (const struct sockaddr*)&socket_address, sizeof(socket_address)) == -1) {
+			perror("sendto() failed");
+			continue;
+		}
+	}
+	fprintf(stderr, "Sent pings to gateway, waiting for reply...\n");
 
 	/* Get the subnet id from the first received arp packet */
 	do {
@@ -142,11 +235,12 @@ int main(int argc, char* argv[]) {
 			}
 		}
 	} while(ntohs(eth_header->ether_type) != ETHERTYPE_ARP
-	     || ntohs(arp_header->ea_hdr.ar_op) != ARPOP_REPLY
-	     || memcmp(arp_header->arp_spa, dst_ip, 4) != 0);
-	memcpy(sw_mac, arp_header->arp_sha, ETH_ALEN);
-	fprintf(stderr, "Got ARP reply from gateway at %02x:%02x:%02x:%02x:%02x:%02x\n",
-	       sw_mac[0], sw_mac[1], sw_mac[2], sw_mac[3], sw_mac[4], sw_mac[5]);
+	     || ntohs(arp_header->ea_hdr.ar_op) != ARPOP_REQUEST
+	     || memcmp(arp_header->arp_spa, sw_ip, 4) != 0
+	     || memcmp(arp_header->arp_sha, sw_mac, ETH_ALEN) != 0);
+	memcpy(src_ip, arp_header->arp_tpa, 4);
+	fprintf(stderr, "Got ARP reply from gateway at:\n");
+	fprintf(stdout, "%d.%d.%d.%d\n", src_ip[0], src_ip[1], src_ip[2], src_ip[3]);
 
 	close(sock);
 	return EXIT_SUCCESS;
