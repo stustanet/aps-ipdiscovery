998d4f040fc89b1c10f3d2fde9df55abf0e49dc1
Markus Hefele <markus.hefele@stusta.net>
2014-10-10 22:46:12 +0200

    ensure, that we got an ARP from a StuSta address

--- ip_discovery.c
+++ ip_discovery.c
@@ -75,6 +75,7 @@ int main(int argc, char* argv[]) {
 	 * be up to ensure, we only get meaningful ARPs. From the first received ARP
 	 * packet we take the subnet id x and from there on assume we are operating
 	 * in 10.150.x.0 */
+        do {
 	do {
 		memset(ether_frame, 0, ETH_FRAME_LEN);
 		if(recv(sock, ether_frame, ETH_FRAME_LEN, 0) == -1) {
@@ -86,8 +87,10 @@ int main(int argc, char* argv[]) {
 			}
 		}
 	} while(ntohs(eth_header->ether_type) != ETHERTYPE_ARP);
+        } while((ntohs(arp_header->arp_spa[0]) != 10) & (ntohs(arp_header->arp_spa[1]) != 150));
 	subnet_id = arp_header->arp_spa[2];
 	fprintf(stderr, "Got ARP, assuming 10.150.%u.0 subnet.\n", subnet_id);
+	fprintf(stderr, "Got ARP from %u.%u.%u.%u.\n", arp_header->arp_spa[0], arp_header->arp_spa[1], arp_header->arp_spa[2], arp_header->arp_spa[3]);
 
 	/* Step 2: Get the next gateways MAC address.
 	 * We send an ARP from 10.150.x.240 to the gateway at 10.150.x.254 to get
