410b63d78756a591bbaf1ffb6ec4c762d0a487cb
mbuhl <git@moritzbuhl.de>
2020-03-05 00:03:43 +0100

    set pointer content not pointer

--- ip_discovery.c
+++ ip_discovery.c
@@ -97,7 +97,7 @@ ifname_from_file(void)
 		err(EXIT_FAILURE, "fclose() failed");
 
 	if ((p = strchr(my_if_name, '\n')) != NULL) {
-		p = '\0';
+		*p = '\0';
 	} else {
 		my_if_name[ifname_len] = '\0';
 	}
